a7059d243703f4e7d56f8b49f38a5d1bb56a96d6
Ainoiv <ainoiv@icloud.com>
2017-10-11 17:32:46 -0700

    first commit

---
+++ dojoMail/dojoMail/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Dojo Mail';
+  emails = [
+    {email: "bill@gates.com", important: true, subject: "New Windows", content: "Windows XI will launch in 2100."},
+    {email: "ada@lovelace.com", important: true, subject: "Programming", content: "Enchantress of Numbers"},
+    {email: "john@carmac.com", important: false, subject: "Updated Algo", content: "New algorithm for shadow volumes."},
+    {email: "gabe@newel.com", important: false, subject: "HL3!", content: "Just kidding"},
+  ]
+}
